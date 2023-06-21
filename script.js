cf2c46adcc1212f624b7be5a4ee1e1c3b02fec11
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-22 01:01:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,19 @@ console.log(res);
 
 */
 
-fetch('https://dummyjson.com/products/1')
+fetch('https://dummyjson.com/products')
     .then((response) => {
     console.log(response);
    return response.json();
   // return response.text();
 })
+    .then(({ products }) => {
+   // console.log(products);
+   return fetch('https://dummyjson.com/products/' + products[0].id);
+})
+    .then((response) => {
+       return response.json();
+})
     .then((data) => {
-    console.log(data);
-});
+        console.log(data);
+    })
